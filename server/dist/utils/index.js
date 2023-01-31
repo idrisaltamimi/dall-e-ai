2a799873e4d2e20b2a8cfcd3d1182ad50a840907
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-01-31 15:01:17 +0400

    remove build

--- server/dist/utils/index.js
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv';
-import path from 'path';
-export const dotenvConfig = (__filename, envPath) => {
-    const __dirname = path.dirname(__filename);
-    dotenv.config({ path: __dirname + envPath });
-};
-//# sourceMappingURL=index.js.map
